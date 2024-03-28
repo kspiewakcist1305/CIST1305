912b08711cd9b30fb414fee56e6519c610ef0777
Krystian Spiewak <kspiewa3698@student.gwinnetttech.edu>
2024-03-27 20:16:10 -0400

    update

---
+++ week14/pass_writer.py
@@ -0,0 +1,30 @@
+# password = input("create a password? ")
+def view():
+    print()
+    with open("passwords.txt", "r") as f:
+        for line in f.readlines():
+            user, password = line.split(" | ")
+            print(f"Account: {user} Password: {password}")
+
+
+def add():
+    name = input("Account Name: ")
+    password = input("Password: ")
+
+    # with allows our code to auto close
+    with open("passwords.txt", "a") as f:
+        f.write(name + " | " + password + "\n")
+
+while True:
+    mode = input("Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower()
+
+    if mode == "q":
+        break
+
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid mode.")
+        continue
